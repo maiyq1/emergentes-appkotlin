1b6fdec6f738efb87daf60bcd2c9846aa16bff45
Mijael Yen <mijael.yq2002@gmail.com>
2024-06-07 21:12:49 -0500

    tb2

---
+++ app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.xperiencelabs.arapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.MediaPlayer
+import android.net.Uri
+import android.opengl.Visibility
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.isGone
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.ar.core.Config
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.AugmentedImageNode
+import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.material.setExternalTexture
+import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
+import io.github.sceneview.node.VideoNode
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sceneView: ArSceneView
+    lateinit var placeButton: ExtendedFloatingActionButton
+    lateinit var stopButton: ExtendedFloatingActionButton
+    private lateinit var modelNode: ArModelNode
+    private lateinit var videoNode: VideoNode
+    private lateinit var mediaPlayer:MediaPlayer
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
+            this.lightEstimationMode = Config.LightEstimationMode.DISABLED
+        }
+
+        placeButton = findViewById(R.id.place)
+
+        placeButton.setOnClickListener {
+            placeModel()
+        }
+
+        /*
+        videoNode = VideoNode(sceneView.engine, scaleToUnits = 0.7f, centerOrigin = Position(y=-4f), glbFileLocation = "models/plane.glb", player = mediaPlayer, onLoaded = {_,_ ->
+            mediaPlayer.start()
+        })
+        */
+
+        modelNode = ArModelNode(sceneView.engine,PlacementMode.INSTANT).apply {
+            loadModelGlbAsync(
+                glbFileLocation = "models/wooden_table.glb",
+                scaleToUnits = 1f,
+                centerOrigin = Position(-0.5f)
+
+            )
+            {
+                sceneView.planeRenderer.isVisible = true
+                val materialInstance = it.materialInstances[0]
+            }
+            onAnchorChanged = {
+                placeButton.isGone = it != null
+            }
+
+        }
+        sceneView.addChild(modelNode)
+        //modelNode.addChild(videoNode)
+
+    }
+
+   private fun placeModel(){
+       modelNode.anchor()
+
+       sceneView.planeRenderer.isVisible = false
+
+   }
+
+    /*
+    CONSIDERAR...
+
+    - DICCIONARIO DE APIS/SERVICIOS
+    - QUÉ DEVOLVERÁ (ALTURA,PESO,TAMAÑO)
+    - ACTUALIZAR DIAGRAMAS
+    -
+     */
+
+}
